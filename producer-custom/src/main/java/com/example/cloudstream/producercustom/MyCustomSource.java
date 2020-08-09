e3c900654b8c3ad8337a68d0e211b2721c273284
Harisson Rafael <harissonrafael@DESKTOP-KFG0GF0>
2020-08-08 21:42:47 -0300

    Custom producer implementations

---
+++ producer-custom/src/main/java/com/example/cloudstream/producercustom/MyCustomSource.java
@@ -0,0 +1,13 @@
+package com.example.cloudstream.producercustom;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface MyCustomSource {
+
+    @Output("default-channel")
+    public MessageChannel sendMessageDefaultChannel();
+
+    @Output("custom-channel")
+    public MessageChannel sendMessageCustomChannel();
+}
